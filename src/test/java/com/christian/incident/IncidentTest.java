d4caf274e2717a9eb17423c68b014e02ab17cddd
Christian Lopes <christianx111.ls@gmail.com>
2026-02-09 21:36:59 -0300

    Implementaçao do teste se o Incidente tem informaçoes invalidas, retornar o codigo 422.

--- src/test/java/com/christian/incident/IncidentTest.java
+++ src/test/java/com/christian/incident/IncidentTest.java
@@ -3,6 +3,7 @@ package com.christian.incident;
 
 import com.christian.incident.web.dto.request.IncidentCreateDto;
 import com.christian.incident.web.dto.response.IncidentResponseDto;
+import com.christian.incident.web.exception.ErrorMessage;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,6 +19,7 @@ public class IncidentTest {
     WebTestClient testClient;
 
     @Test
+    // Test - CreateIncident
     public void createIncident_WithValidInformation_ReturnCode201(){
      IncidentResponseDto responseBody =  testClient
                 .post()
@@ -36,6 +38,39 @@ public class IncidentTest {
     org.assertj.core.api.Assertions.assertThat(responseBody.location()).isEqualTo("Apartamento 610");
     org.assertj.core.api.Assertions.assertThat(responseBody.status()).isEqualTo("OPEN");
     org.assertj.core.api.Assertions.assertThat(responseBody.createdAt()).isNotNull();
+    }
 
+    @Test
+    // Testa - CreateIncident
+    public void createIncident_WithInvalidInformationIncident_ReturnCode422(){
+        ErrorMessage responseBody =  testClient
+                .post()
+                .uri("/api/v1/incident")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new IncidentCreateDto("", "Apartamento 610"))
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessage.class)
+                .returnResult()
+                .getResponseBody();
+
+    org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+    org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+
+    //Testa - Se o Incident tem 6 caracteres.
+    responseBody = testClient
+            .post()
+            .uri("/api/v1/incident")
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValue(new IncidentCreateDto("lampa", "Apartamento 610"))
+            .exchange()
+            .expectStatus().isEqualTo(422)
+            .expectBody(ErrorMessage.class)
+            .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
     }
 }
+
+
