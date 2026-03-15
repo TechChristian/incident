9728b32fbca9b903e8fe8ac31d5741e1646978e8
Christian Lopes <christianx111.ls@gmail.com>
2026-03-15 14:45:56 -0300

    verificacoes de token, claims e refatoracao do Bearer criados!

--- src/main/java/com/christian/incident/Jwt/JwtUtils.java
+++ src/main/java/com/christian/incident/Jwt/JwtUtils.java
@@ -1,9 +1,7 @@
 package com.christian.incident.Jwt;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
-
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
@@ -24,19 +22,21 @@ public class JwtUtils {
     public static final long EXPIRE_HOURS = 0;
     public static final long EXPIRE_MINUTES = 2;
 
-    private JwtUtils(){
+    private JwtUtils() {
     }
 
-    public static Key generateKey(){
-        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));}
+    public static Key generateKey() {
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+    }
 
-    private static Date toExpireDate(Date start){
+    private static Date toExpireDate(Date start) {
         long expirationMillis =
-                    (EXPIRE_DAYS * 24L * 60 * 60 * 1000) +
-                    (EXPIRE_HOURS * 60L * 60 * 1000) +
-                    (EXPIRE_MINUTES * 60L * 1000);
+                (EXPIRE_DAYS * 24L * 60 * 60 * 1000) +
+                        (EXPIRE_HOURS * 60L * 60 * 1000) +
+                        (EXPIRE_MINUTES * 60L * 1000);
         return new Date(start.getTime() + expirationMillis);
     }
+
     public static JwtToken createToken(String username, String role) {
         Date issueAt = new Date(); // Data de criação do token
         Date limit = toExpireDate(issueAt); // Data de expiração do token
@@ -49,5 +49,38 @@ public class JwtUtils {
                 .claim("role", role)
                 .compact();
         return new JwtToken(token);
+    }
+
+    // -> Extrai informações do Token
+    public static Claims getClaimsFromToken(String token) {
+        try {
+            return Jwts.parserBuilder()
+                    .setSigningKey(generateKey())
+                    .build()
+                    .parseClaimsJws(refactorTokenBearer(token))
+                    .getBody();
+        } catch (JwtException ex) {
+            log.error("Invalid JWT Token: {}", ex.getMessage());
+        }
+        return null;
+    }
+
+    // -> Obtem username armazenado no Token.
+    public static String getUsernameFromToken(String token){
+        Claims claims = getClaimsFromToken(token);
+
+        return claims !=null ? claims.getSubject() : null;
+    }
+
+    public static boolean isTokenValid(String token){
+        return getClaimsFromToken(token) != null;
+    }
+
+    // -> Remove o "Bearer" ou seja o prefixo do tipo de Autenticação.
+    private static String refactorTokenBearer(String token) {
+        if (token.startsWith(JWT_BEARER)) {
+            return token.substring(JWT_BEARER.length());
+        }
+        return token;
     }
 }
