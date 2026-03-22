d1baa85d384ef853cc37dc58377f89cd1196a4b8
Christian Lopes Ramos <christian@MacBook-Air-de-Christian.local>
2026-03-22 00:44:56 -0300

    Classe de filtros para interceptar as requsicoes do usuario e testar elas

---
+++ src/main/java/com/christian/incident/Jwt/JwtAuthorizationFilter.java
@@ -0,0 +1,55 @@
+package com.christian.incident.Jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@NoArgsConstructor
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+    private JwtUserDetailsService detailsService;
+
+    //-> metodo que vai interceptar as requisicoes
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        final String token = request.getHeader(JwtUtils.JWT_AUTHORIZATION);
+
+        if(token == null || !token.startsWith(JwtUtils.JWT_BEARER)){
+            log.info("JWT is null or has not been initialized with a 'Bearer'.");
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        if( !JwtUtils.isTokenValid(token)){
+            log.warn("JWT is invalid.");
+            filterChain.doFilter(request,response);
+            return;
+        }
+        String username = JwtUtils.getUsernameFromToken(token);
+        toAuthentication(request, username);
+
+        filterChain.doFilter(request, response);
+    }
+
+    private void toAuthentication(HttpServletRequest request, String username) {
+        UserDetails userDetails = detailsService.loadUserByUsername(username);
+
+        UsernamePasswordAuthenticationToken authenticationToken = UsernamePasswordAuthenticationToken
+                .authenticated(userDetails, null, userDetails.getAuthorities());
+        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+    }
+}
